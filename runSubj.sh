ec34b92bcf3056b1f0ef021c4e9b73205c2ea3d1
Forrest <forrest.c.koch@gmail.com>
2018-05-28 14:52:52 +1000

    add dti_fit back into pipe

--- runSubj.sh
+++ runSubj.sh
@@ -9,6 +9,7 @@ cd "$SUBJ"
 #$DIR/scripts/processing/parcellate.sh > parcellate.log 2>&1 & 
 $DIR/scripts/processing/distortion_correction.sh>distortion_correction.log 2>&1 
 $DIR/scripts/processing/coregister.sh > coregister.log 2>&1 
+$DIR/scripts/processing/fit_tensors > fit_tensors.log 2>&1 
 $DIR/scripts/processing/calculate_noddi.sh > calculate_noddi.log 2>&1
 $DIR/scripts/processing/tractography.sh > tractography.log 2>&1
 #$DIR/scripts/processing/get_connectome.sh > getConnectome.log 2>&1

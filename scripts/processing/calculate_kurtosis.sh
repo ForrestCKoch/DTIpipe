4171cfa83c6f4e2a4564b44c852170af9bbb4c63
Forrest Koch <forrest.koch@unsw.edu.au>
2018-06-12 15:06:52 +1000

    bug fixes

--- scripts/processing/calculate_kurtosis.sh
+++ scripts/processing/calculate_kurtosis.sh
@@ -10,7 +10,7 @@ BVECS="$SUBJECT_DIR/workdir/distortion_correction/bvecs_ec"
 cd workdir
 mkdir -p kurtosis_calculation
 cd kurtosis_calculation
-mkdir outdir
+mkdir -p outdir
 
 echo 'building bvecs table...'
 $BASE/scripts/helpers/generate_dke_bvecs_table.py $BVECS
@@ -18,8 +18,7 @@ cp $SUBJECT_DIR/workdir/distortion_correction/eddy_corrected.nii.gz tmp.nii.gz
 
 echo 'preparing dti volumes... '
 fslsplit tmp
-rm vol0020.nii.gz vol0040.nii.gz vol0060.nii.gz vol0080.nii.gz vol0100.nii.gz\ 
-   vol0120.nii.gz vol0140.nii.gz 
+rm vol0020.nii.gz vol0040.nii.gz vol0060.nii.gz vol0080.nii.gz vol0100.nii.gz vol0120.nii.gz vol0140.nii.gz 
 echo 'merging dti volumes ...'
 fslmerge -t dti vol*
 
@@ -27,7 +26,7 @@ rm tmp.nii.gz
 rm vol*
 
 echo 'unzipping dti ...'
-gunzip dti
+gunzip dti.nii.gz
 
 mv dti.nii outdir 
 

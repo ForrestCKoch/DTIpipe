d38769e0519398bf8f19ac2da7dac38a256d372e
Forrest Koch <forrest.koch@unsw.edu.au>
2018-06-19 14:09:23 +1000

    add qc script

---
+++ scripts/qc/coreg_qc.sh
@@ -0,0 +1,24 @@
+#/bin/sh
+
+# A little hack to make sure paths are as we expect
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE="$(readlink -f $SCRIPT_DIR/../..)"
+
+START="$(pwd)"
+
+mkdir -p ../QC/B0_to_T1_coreg
+
+for i in *; do
+	echo "Preparing $i ..."
+	b0=$i/workdir/coregistration/epi_reg	
+	wmseg=$i/workdir/coregistration/wmseg
+	overlay 1 1 $b0 -A $wmseg 1 1 ../QC/B0_to_T1_coreg/$i
+done
+
+cd ../QC/B0_to_T1_coreg
+echo "Calling slicesdir ..."
+slicesdir *
+
+cd $START
+
+

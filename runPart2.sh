2661581908749b952b2985eb12657f75c3ab7553
Forrest Koch <forrest.koch@unsw.edu.au>
2018-06-28 14:19:58 +1000

    fixing cluster fuckup

--- runPart2.sh
+++ runPart2.sh
@@ -11,18 +11,18 @@ mkdir -p logs
 #$DIR/scripts/processing/calculate_noddi.sh > logs/calculate_noddi.log 2>&1
 #echo "Calculating Kurtosis"
 #$DIR/scripts/processing/calculate_kurtosis.sh > logs/calculate_kurtosis.log 2>&1
-echo "Fitting tensors"
-$DIR/scripts/processing/fit_tensors.sh> logs/fit_tensors.log 2>&1
-echo "Preparing WMH vs NAWM"
-$DIR/scripts/processing/prepare_wmh_vs_nawm.sh > logs/prep_wmh.log 2>&1
-echo "Preparing WMH Shells"
-$DIR/scripts/processing/prep_wmh_shells.sh > logs/prep_shells.log 2>&1
+#echo "Fitting tensors"
+#$DIR/scripts/processing/fit_tensors.sh> logs/fit_tensors.log 2>&1
+#echo "Preparing WMH vs NAWM"
+#$DIR/scripts/processing/prepare_wmh_vs_nawm.sh > logs/prep_wmh.log 2>&1
+#echo "Preparing WMH Shells"
+#$DIR/scripts/processing/prep_wmh_shells.sh > logs/prep_shells.log 2>&1
 echo "Preparing Clusters"
 $DIR/scripts/processing/prep_clusters.sh > logs/prep_clusters.log 2>&1
-echo "Comparing WMH vs NAWM"
-$DIR/scripts/processing/compare_wmh_vs_nawm.sh > logs/comp_wmh.log 2>&1
-echo "Comparing Shells"
-$DIR/scripts/processing/compare_wmh_shells.sh > logs/comp_shells.log 2>&1
+#echo "Comparing WMH vs NAWM"
+#$DIR/scripts/processing/compare_wmh_vs_nawm.sh > logs/comp_wmh.log 2>&1
+#echo "Comparing Shells"
+#$DIR/scripts/processing/compare_wmh_shells.sh > logs/comp_shells.log 2>&1
 echo "Measuring Clusters"
 $DIR/scripts/processing/measure_clusters.sh > logs/measure_clusters.log 2>&1
 cd $STARTDIR
--- scripts/processing/prep_clusters.sh
+++ scripts/processing/prep_clusters.sh
@@ -5,9 +5,9 @@ BASE="$(readlink -f $SCRIPT_DIR/../..)"
 
 SUBJECT_DIR="$(pwd)"
 
-# we should alter this to ultimately use original space possibly?
 WMH_MASK="$SUBJECT_DIR/workdir/wmh_vs_nawm/wmh_mask_flair_space"
 FLAIR_TO_DWI="$SUBJECT_DIR/workdir/wmh_vs_nawm/flair_to_dwi.mat"
+DTI="$SUBJECT_DIR/workdir/distortion_correction/eddy_corrected"
 
 cd workdir
 mkdir -p clusters
@@ -22,9 +22,9 @@ cluster --in=$WMH_MASK --thresh=1 --oindex=index_flair --osize=size_flair > clus
 lines=$(cat cluster.txt|tail -n $(( $(cat cluster.txt|wc -l) -1)))
 
 #convert cluster results to dwi
-flirt -in index_flair -ref $SUBJECT_DIR/dti -applyxfm -init $FLAIR_TO_DWI\
+flirt -in index_flair -ref $DTI -applyxfm -init $FLAIR_TO_DWI\
 	-out index_dwi -interp nearestneighbour
-flirt -in size_flair -ref $SUBJECT_DIR/dti -applyxfm -init $FLAIR_TO_DWI\
+flirt -in size_flair -ref $DTI -applyxfm -init $FLAIR_TO_DWI\
 	-out size_dwi -interp nearestneighbour
 
 while read c; do
@@ -41,22 +41,9 @@ while read c; do
     fslmaths index_dwi -thr $id -uthr $id \
 		-bin dwi_clusters/cluster_${id}
 
-    gunzip -f dwi_clusters/cluster_${id}.nii.gz
-
+    gunzip -f dwi_clusters/cluster_${id}.nii.gz 
 
 done <<< "$lines"
-
-    # this is time consuming, so include it in prep stage
-#    matlab -nosplash -nodesktop -r "\
-#    fp = fopen('cluster_${id}_props.txt','w');\
-#    mask = readnifti('cluster_${id}_${size}.nii');\
-#    stats = regionprops3(mask,{'Volume' 'SurfaceArea'});\
-#    v = stats.Volume;\
-#    s = stats.SurfaceArea;\
-#    sphericity = ((pi^(1/3))*(6*stats.Volume)^(2/3))/stats.SurfaceArea;\
-#    fprintf(fp,'%f,%f,%f',stats.Volume,stats.SurfaceArea,sphericity);\
-#    fp.close();\
-#    "</dev/null > /dev/null 2>&1
 
 matlab -nosplash -nodesktop -r "\
 \

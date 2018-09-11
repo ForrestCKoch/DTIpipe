d4645e45c50730d4d4de86c8dea5031e7c025d73
Forrest Koch <forrest.koch@unsw.edu.au>
2018-09-11 15:01:54 +1000

    only calculate fsl dti for 700 and 1000 bvals

--- scripts/processing/fit_tensors.sh
+++ scripts/processing/fit_tensors.sh
@@ -15,18 +15,16 @@ cd response_maps
 mkdir -p fsl_dti_maps
 mkdir -p fsl_kurt_maps
 
+# fit the kurtosis model
 echo "Using 0, 700, 1000, 2800 b-vals"
-dtifit -k $DTI -r $BVECS -b $BVALS -o fsl_dti_maps/dti -m $MASK
 dtifit --kurt -k $DTI -r $BVECS -b $BVALS -o fsl_kurt_maps/kurt -m $MASK
 # create the AD and RD metrics
-fslmaths fsl_dti_maps/dti_L1 -add fsl_dti_maps/dti_L2 -div 2 \
-	fsl_dti_maps/dti_RD
 cp fsl_dti_maps/dti_L1 fsl_dti_maps/dti_AD
 fslmaths fsl_kurt_maps/dti_L1 -add fsl_kurt_maps/kurt_L2 -div 2 \
 	fsl_kurt_maps/kurt_RD
-#cp fsl_kurt_maps/kurt_L1 fsl_kurt_maps/kurt_AD
 
-for i in 700 1000 2800 700_1000; do
+# fit the dti model using only shells < 2000
+for i in 700_1000; do
 	mkdir -p fsl_$i
 	echo "Using 0, $(echo $i|tr '_' ',') b-vals"
 	dwiextract -shell 0,$(echo $i|tr '_' ',')  -fslgrad $BVECS $BVALS $DTI - |\
